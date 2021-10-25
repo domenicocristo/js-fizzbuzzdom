2cf7080026110c1019bd191bdfa1471bd64ae506
MyUsername <my@email.com>
2021-10-25 16:44:51 +0200

    commit

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const contBlocks = document.getElementById("container-blocks");
  for (var i = 1; i <= 100; i++) {
     contBlocks.innerHTML += `<div class="blocks">${i}</div>`; 
     if (i % 15 == 0) 
-        contBlocks.innerHTML += `<div class="blocks">fizz${i}</div>`;
+        contBlocks.innerHTML += `<div class="blocks red">${i}</div>`;
     else if (i % 3 == 0) 
-        contBlocks.innerHTML += `<div class="blocks">${i}</div>`;
+        contBlocks.innerHTML += `<div class="blocks green">${i}</div>`;
     else if (i % 5 == 0) 
-        contBlocks.innerHTML += `<div class="blocks">${i}</div>`;
+        contBlocks.innerHTML += `<div class="blocks yellow">${i}</div>`;
     else console.log(i);
 }
