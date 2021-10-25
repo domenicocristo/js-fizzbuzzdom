4dceeead25653c601602b98ebe15864c68bb5f05
MyUsername <my@email.com>
2021-10-25 16:50:30 +0200

    fine

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const contBlocks = document.getElementById("container-blocks");
 
  for (var i = 1; i <= 100; i++) {
-    contBlocks.innerHTML += `<div class="blocks">${i}</div>`; 
     if (i % 15 == 0) 
         contBlocks.innerHTML += `<div class="blocks red">${i}</div>`;
     else if (i % 3 == 0) 
         contBlocks.innerHTML += `<div class="blocks green">${i}</div>`;
     else if (i % 5 == 0) 
         contBlocks.innerHTML += `<div class="blocks yellow">${i}</div>`;
-    else console.log(i);
+    else 
+    contBlocks.innerHTML += `<div class="blocks">${i}</div>`;
 }
